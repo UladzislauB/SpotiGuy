b6127d2e937a982c81c4a180f0e277dbdecb749e
Uladzislau <devuladblr@gmail.com>
2020-09-29 23:50:14 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- spotiguy/settings.py
+++ spotiguy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z9-0jg(6=(xf-!dr%-*pk29c6n5l87
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vast-oasis-76347.herokuapp.com', '127.0.0.1']
 
 # Custom user
 AUTH_USER_MODEL = 'authentication.User'
